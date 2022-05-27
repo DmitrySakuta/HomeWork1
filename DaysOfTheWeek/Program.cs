575938c441d14ea17e852a1baa940da0061b2a93
DimaSakuta <dima291093@gmail.com>
2022-05-27 11:02:34 +0300

    First commit

---
+++ DaysOfTheWeek/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace DaysOfTheWeek
+{
+    class Program
+    {
+
+        static string getNameDays(int numberDay) 
+        {                      
+            switch (numberDay)
+            {
+                case 1: return "Monday"; 
+                case 2: return "Tuesday"; 
+                case 3: return "Wednesday"; 
+                case 4: return "Thursday"; 
+                case 5: return "Friday"; 
+                case 6: return "Saturday"; 
+                case 7: return "Sunday";
+
+                default: return "Day is not defined!";
+            }
+
+        }
+
+        static void outputConsole() 
+        {
+            byte numberDay;
+
+            do
+            {
+                Console.WriteLine("Please, enter a day number!");
+                if (!byte.TryParse(Console.ReadLine(), out numberDay)) {outputConsole(); return;}
+           
+            }
+            while (7<numberDay || numberDay<1);
+
+            Console.WriteLine($"Day of the week: {getNameDays(numberDay)} \n");
+            string anser;
+
+            do
+            {
+                Console.WriteLine("Do you wanna repeat? Enter Y/N.");
+                anser = Console.ReadLine();
+                
+                anser = anser.ToUpper();
+            }
+            while (anser != "Y" && anser != "N");
+            if (anser == "Y") outputConsole();
+            else Console.WriteLine("The job is done!");
+
+        }  
+
+        static void Main(string[] args)
+        {
+            outputConsole();
+        }
+    }
+}
---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static long MultiplicationMethod(long number)
+        {
+            for(int i=0; i<10; i++) 
+            {
+                Console.WriteLine($"{number}x{i}={number*i}");
+            }
+          
+            return 5;
+        }
+        
+        static void outputConsole()
+        {
+            long number;
+            Console.WriteLine("Please, enter a number!");
+            if (!long.TryParse(Console.ReadLine(), out number)) { outputConsole(); return; }
+            MultiplicationMethod(number);
+            string anser;
+            do
+            {
+                Console.WriteLine("Do you wanna repeat? Enter Y/N.");
+                anser = Console.ReadLine();
+
+                anser = anser.ToUpper();
+            }
+            while (anser != "Y" && anser != "N");
+            if (anser == "Y") outputConsole();
+            else Console.WriteLine("The job is done!");
+
+        }
+
+        static void Main(string[] args)
+        {
+          
+            outputConsole();
+        }
+    }
+}
